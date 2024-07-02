ab183820275fddf587d422b8b8154ff453e3ff24
Tommaso De Tommaso <tommaso.detommaso@protonmail.com>
2024-07-02 13:15:12 +0200

    Polished code and now fully working

--- nmap-clone.py
+++ nmap-clone.py
@@ -2,93 +2,133 @@ import struct
 import threading
 import socket
 import tkinter
+import time
 
-# Calcola il checksum
-def get_checksum(source):
-    checksum = 0
-    for i in range(0, len(source), 2):
-        checksum += (source[i] << 8) + source[i + 1]
-    checksum = (checksum >> 16) + (checksum & 0xFFFF)
-    checksum = ~checksum & 0xFFFF
-    return checksum
+class utils:
+    class Status:
+        ERROR = -1
+        ONLINE = 0
+        OFFLINE = 1
 
-# Controlla se un singolo host è online utilizzando il protocollo ICMP
-def check_host(hostname):
-    # Creazione del socket
-    icmp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    # Calcola il checksum come specificato in RFC1071
+    def get_checksum(source):
+        checksum = 0
+        for i in range(0, len(source), 2):
+            checksum += (source[i] << 8) + source[i + 1]
+        checksum = (checksum >> 16) + (checksum & 0xFFFF)
+        checksum = ~checksum & 0xFFFF
+        return checksum
+    
+    # Ritorna un pacchetto ICMP
+    def get_icmp_packet():
+        # Creazione del pacchetto ICMP
+        icmp_packet = struct.pack('!BBHHH', 8, 0, 0, 0, 1) + b'pingdata'
+        # Calcolo del checksum del pacchetto ICMP
+        icmp_checksum = utils.get_checksum(icmp_packet)
+        # Aggiunta del checksum al pacchetto stesso
+        icmp_packet = struct.pack('!BBHHH', 8, 0, icmp_checksum, 0, 1) + b'pingdata'
+        return icmp_packet
+    
+    # Controlla se un host è online utilizzando il protocollo ICMP
+    def check_host(hostname):
+        # Creazione del socket
+        icmp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+        # Creazione del pacchetto ICMP
+        icmp_packet = utils.get_icmp_packet()
+        # Invio del pacchetto ICMP all'host
+        icmp_socket.sendto(icmp_packet, (hostname, 0))
 
-    # Creazione del pacchetto ICMP
-    icmp_packet = struct.pack('!BBHHH', 8, 0, 0, 0, 1) + b'pingdata'
+        try:
+            # Se c'è risposta, viene recuperata
+            response, _ = icmp_socket.recvfrom(1024)
+            # Si prende il parametro "type" dal pacchetto di risposta 
+            icmp_type = struct.unpack('!B', response[20:21])[0]
+            # Si chiude il socket
+            icmp_socket.close()
+            # Si verifica se la risposta è 0, "echo reply"
+            if icmp_type == 0:
+                return utils.Status.ONLINE
+            else:
+                return utils.Status.OFFLINE
+        except Exception:
+            return utils.Status.ERROR
+    
+class Host:
+    # Costruttore di Host
+    def __init__(self, hostname, timeout, on_status_updated):
+        self.hostname = hostname
+        self.timeout = timeout
+        self.on_status_updated = on_status_updated
 
-    # Calcolo del checksum del pacchetto ICMP creato
-    icmp_checksum = get_checksum(icmp_packet)
+    # Esegue un singolo ping
+    def check(self):
+        self.status = utils.check_host(self.hostname)
+        self.on_status_updated(self)
+    
+    # Esegue continui ping nel thread corrente
+    def check_always(self):
+        while True:
+            self.check()
+            time.sleep(self.timeout)
 
-    # Aggiunta del checksum al pacchetto stesso
-    icmp_packet = struct.pack('!BBHHH', 8, 0, icmp_checksum, 0, 1) + b'pingdata'
+    # Esegue continui ping in un nuovo thread
+    def check_always_threaded(self):
+        self.linked_thread = threading.Thread(target=self.check)
+        self.linked_thread.daemon = True
+        self.linked_thread.start()
 
-    # Invio del pacchetto ICMP all'hostname
-    icmp_socket.sendto(icmp_packet, (hostname, 0))
+# Gestisce l'aggiornamento di una label in base al cambio di stato di un Host
+def update_label(label:tkinter.Label, host: Host):
+    if(host.status == utils.Status.ONLINE):
+        label.config(text=host.hostname + " - online")
+    elif(host.status == utils.Status.OFFLINE):
+        label.config(text=host.hostname + " - offline")
+    elif(host.status == utils.Status.ERROR):
+        label.config(text=host.hostname + " - error")
 
-    try:
-        # Se c'è risposta, viene recuperata
-        response, _ = icmp_socket.recvfrom(1024)
-        # Si prende il parametro "type" dal pacchetto di risposta 
-        icmp_type = struct.unpack('!B', response[20:21])[0]
-        # Si chiude il socket
-        icmp_socket.close()
-
-        # Si verifica se la risposta è 0, "echo reply"
-        if icmp_type == 0:
-            return True
-    except socket.error:
-        pass
-
-    return False
-
-# Questo metodo verrà usato dai thread.
-# Controlla continumente lo stato di un singolo host, eseguendo delle lambda in base a se è online o no
-def continuous_host_check(hostname, success_lambda, failure_lambda):
-    while True:
-        success = check_host(hostname)
-        if success:
-            success_lambda()
-        else:
-            failure_lambda()
-
-# -------------------------------------------------------------------------------------------------------------------------
-
-def update_host():
-    pass
-
+# Gestisce l'aggiunta di un nuovo host
 def add_host(event = None):
-    new_thread = threading.Thread(target=continuous_host_check, args=(input_ip.get(), lambda: print("ping pong"), lambda: print("fail")))
-    new_thread.daemon = True
-    new_thread.start()
-    thread_list[input_ip.get()] = new_thread
-    hosts_list.insert(tkinter.END, input_ip.get())
+    # Prende l'hostname inserito nell'Entry
+    hostname = input_hostname.get()
+
+    # Viene creata una nuova Label che mostrera lo stato dell'host
+    new_label = tkinter.Label(hosts_frame, text=hostname + " - waiting")
+    new_label.pack()
+
+    # Viene creato un Host a cui si passa una lambda che gestirà l'aggiornamento della label
+    new_host = Host(hostname, 2, lambda host: update_label(new_label, host))
+    # Si inizia a fare il check continuo in un altro thread sull'Host
+    new_host.check_always_threaded()
+
+    # Si aggiunge l'Host appena creato ad una lista
+    hosts_list.append(new_host)
+    # Si svuota il valore inserito nell'Entry
+    input_hostname.set("")
 
 # Main
-thread_list = {}
+hosts_list = []
 
 window = tkinter.Tk()
 window.title("nmap-clone")
 window.minsize(400, 300)
 window.maxsize(400, 300)
 
-hosts_list = tkinter.Listbox(window, width=30)
-hosts_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+hosts_frame = tkinter.Frame(window, width=50, background="white")
+hosts_frame.pack(side=tkinter.LEFT)
+
+hosts_label = tkinter.Label(hosts_frame, text="Hosts")
+hosts_label.pack()
 
 title_label = tkinter.Label(window, text="Network mapping tool")
 title_label.pack()
 
-input_ip = tkinter.StringVar()
+input_hostname = tkinter.StringVar()
 
-ip_field = tkinter.Entry(window, textvariable=input_ip)
+ip_field = tkinter.Entry(window, textvariable=input_hostname)
 ip_field.bind("<Return>", add_host)
 ip_field.pack()
 
 add_button = tkinter.Button(window, text="Aggiungi", command=add_host)
 add_button.pack()
-
 
 tkinter.mainloop()
