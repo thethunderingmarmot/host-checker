209419f5a607743f8ac2208f307cd35d36eca668
Tommaso De Tommaso <tommaso.detommaso@protonmail.com>
2024-07-02 10:02:44 +0200

    Better function separation for incoming multi-threading

--- nmap-clone.py
+++ nmap-clone.py
@@ -2,7 +2,7 @@ import struct
 import threading
 import socket
 
-def check_host(ip):
+def check_host(hostname):
     icmp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
 
     icmp_packet = struct.pack('!BBHHH', 8, 0, 0, 0, 1) + b'pingdata'
@@ -15,7 +15,7 @@ def check_host(ip):
     icmp_checksum = ~icmp_checksum & 0xFFFF
     icmp_packet = struct.pack('!BBHHH', 8, 0, icmp_checksum, 0, 1) + b'pingdata'
 
-    icmp_socket.sendto(icmp_packet, (ip, 0))
+    icmp_socket.sendto(icmp_packet, (hostname, 0))
 
     try:
         response, _ = icmp_socket.recvfrom(1024)
@@ -28,8 +28,12 @@ def check_host(ip):
 
     return False
 
-success = check_host("google.com")
-if success:
-    print("Ping pong!")
-else:
-    print("Ping...")
+def continuous_host_check(hostname, success_lambda, failure_lambda):
+    while True:
+        success = check_host(hostname)
+        if success:
+            success_lambda()
+        else:
+            failure_lambda()
+
+continuous_host_check("google.it", lambda: print("Ping pong!"), lambda: print("No response!"))
